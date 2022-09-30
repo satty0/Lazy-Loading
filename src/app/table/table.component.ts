5134c6223134c216474137ca691bf46b3a40ea46
satty0 <satish.m.kumar@capgemini.com>
2022-09-30 13:21:09 +0530

    second commit

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LazyLoadEvent } from 'primeng/api';
-import { map, Observable } from 'rxjs';
+import { Observable, pluck } from 'rxjs';
 import { ApiService } from '../api.service';
-// import { User } from '../user';
+import { Datum } from '../user';
 
 @Component({
   selector: 'app-table',
@@ -11,31 +11,18 @@ import { ApiService } from '../api.service';
 })
 export class TableComponent implements OnInit {
 
-  public data$: Observable<any> | undefined; // need to work on <any> -----> <User>
-  loading: boolean = true;
+  public data$: Observable<Datum[]> | undefined; 
   totalRecords: number = 200;
   pageNumber: number = 1;
 
   constructor(private service: ApiService) { }
   
   ngOnInit(): void {
-    this.data$ = this.service.getLazyData(1, 10).pipe(map(res => {
-      this.totalRecords = res.totalPassengers ?? 0;
-      return res.data
-    }))
-    this.loading = true;
-  }
+    this.data$ = this.service.getLazyData(1, 10).pipe(pluck('data'));
+    }
 
-  //for lazy loading 
   loadCustomers(event: LazyLoadEvent) {
-    this.loading = true;
     event.first == 0 ? this.pageNumber = 1 : this.pageNumber = (event.first ?? 10) / (event.rows ?? 10) + 1;
-    this.data$ = this.service.getLazyData(this.pageNumber, event.rows ?? 10).pipe(
-      map(res => {
-        this.totalRecords = res.totalPassengers;
-        this.loading = false;
-        return res.data;
-      })
-    )
-  }
+    this.data$ = this.service.getLazyData(this.pageNumber, event.rows ?? 10).pipe( pluck('data'));
+         }
 }
